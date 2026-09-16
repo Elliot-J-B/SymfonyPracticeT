0a9886c87273b1b11f123acd9a5cb7a5b873ff34
Elliot-J-B <burgeselliot@gmail.com>
2026-09-16 10:48:01 +0200

    [New] Book, Author and Country entities added

---
+++ src/Entity/Book.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ORM\Table(name:'tbl_book')]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $NbrPages = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getNbrPages(): ?int
+    {
+        return $this->NbrPages;
+    }
+
+    public function setNbrPages(int $NbrPages): static
+    {
+        $this->NbrPages = $NbrPages;
+
+        return $this;
+    }
+}
